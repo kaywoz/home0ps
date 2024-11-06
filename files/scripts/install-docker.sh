537223b0efa7299be6537f26c530a4e7a33d1c04
ka <64521603+kaywoz@users.noreply.github.com>
2024-11-06 15:13:46 +0100

    123

--- files/scripts/install-docker.sh
+++ files/scripts/install-docker.sh
@@ -13,4 +13,4 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker
 systemctl enable docker
 sudo usermod -aG docker $USER
-echo "Docker installation complete. Please log out and back in for group changes to take effect."
+echo "Docker installation complete. Please log out and back in for group changes to take effect."O
