130ebab82586bdc469059172e4fe0d0a03f77184
ka <64521603+kaywoz@users.noreply.github.com>
2026-02-23 15:38:32 +0100

    base

--- iac/tofu.tf
+++ iac/tofu.tf
@@ -1,1 +1,12 @@
-123
+terraform {
+  required_providers {
+    hetzner = {
+      source = "opsheaven/hetzner"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "hetzner" {
+  # Configuration options
+}
