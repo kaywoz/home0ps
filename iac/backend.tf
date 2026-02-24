29d44dbe28e1b8eb7dad0b34d283921c2e5bad3c
ka <64521603+kaywoz@users.noreply.github.com>
2026-02-24 16:09:07 +0100

    123

--- iac/backend.tf
+++ iac/backend.tf
@@ -3,6 +3,7 @@ terraform {
     endpoint   = "https://hel1.your-objectstorage.com"
     bucket     = "krypinet-iac"
     key        = "B9VAIBFMAPZA49IYDN3B"
+    region     = "us-east-1"
 
     skip_credentials_validation = true
     skip_region_validation      = true
