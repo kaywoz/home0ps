bcb84e9e83f4cd0dbb2e45364c19e177eef78153
ka <64521603+kaywoz@users.noreply.github.com>
2026-06-10 14:38:46 +0200

    123

--- iac/netbird.tf
+++ iac/netbird.tf
@@ -5,12 +5,3 @@ resource "netbird_dns_zone" "internal" {
   enable_search_domain = true
   distribution_groups  = [netbird_group.example.id]
 }
-
-resource "netbird_network_resource" "example" {
-  network_id  = netbird_network.example.id
-  name        = "shpc-netbird-0101.netbird.cloud"
-  description = "shpc-netbird-0101.netbird.cloud"
-  address     = "172.16.31.10/32"
-  groups      = [ingress]
-  enabled     = true
-}
