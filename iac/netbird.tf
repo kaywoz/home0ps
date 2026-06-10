3d80cdf5afa6c5726419419ba33b8fec4780119e
ka <64521603+kaywoz@users.noreply.github.com>
2026-06-10 14:40:16 +0200

    123

--- iac/netbird.tf
+++ iac/netbird.tf
@@ -1,7 +1,3 @@
-resource "netbird_dns_zone" "internal" {
-  name                 = "sh-internal"
-  domain               = "sh-internal.krypi.net"
-  enabled              = true
-  enable_search_domain = true
-  distribution_groups  = [netbird_group.example.id]
+resource "netbird_group" "ingress" {
+  name = "ingress"
 }
