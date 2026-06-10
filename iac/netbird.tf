c0293a74ba3182febef338b2a25930fb27a78878
ka <64521603+kaywoz@users.noreply.github.com>
2026-06-10 14:49:36 +0200

    123

--- iac/netbird.tf
+++ iac/netbird.tf
@@ -1,3 +1,12 @@
 resource "netbird_group" "ingress" {
   name = "ingress"
+} ## add all regular groups here
+
+resource "netbird_peer" "ingress-netbird" {
+  id                            = "d057h0jl0ubs73cftnp0"
+  ssh_enabled                   = false
+  name                          = "shpc-netbird-0101"
+  inactivity_expiration_enabled = false
+  approval_required             = false
+  login_expiration_enabled      = false
 }
