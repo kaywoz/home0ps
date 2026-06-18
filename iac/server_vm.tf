60a098f822d09eb3d55762c636b02e8f793e9460
ka <64521603+kaywoz@users.noreply.github.com>
2026-06-18 11:01:34 +0200

    123

--- iac/server_vm.tf
+++ iac/server_vm.tf
@@ -1,12 +1,12 @@
 # Create a new server running debian
-resource "hcloud_server" "honeypot" {
-  name        = "hlpa-tpotce-0101"
-  image       = "debian-12"
-  server_type = "cx23"
-  location    = "hel1"
-  ssh_keys    = ["github"]
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-}
+#resource "hcloud_server" "honeypot" {
+ # name        = "hlpa-tpotce-0101"
+ # image       = "debian-12"
+ # server_type = "cx23"
+ # location    = "hel1"
+ # ssh_keys    = ["github"]
+ # public_net {
+  #  ipv4_enabled = true
+  #  ipv6_enabled = true
+  #}
+#}
